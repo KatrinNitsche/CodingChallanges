099cb9049d8faffe471646a72355a184a00ee2fd
Katrin Nitsche <katrin.nitsche@ecomsilver.com>
2021-02-14 14:17:08 +0000

    added FizzBuzz challange

---
+++ src/CodingChallanges/CodingChallanges.Tests/FizzBuzzHelperShould.cs
@@ -0,0 +1,80 @@
+﻿using Xunit;
+
+namespace CodingChallanges.Tests
+{
+    public class FizzBuzzHelperShould
+    {
+        [Fact]
+        public void ReturnAListOfStrings()
+        {
+            var sut = new FizzBuzzHelper();
+            var result = sut.GetList();
+
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+        }
+
+        [Fact]
+        public void ReturnAListOfCorrectStrings()
+        {
+            var sut = new FizzBuzzHelper();
+            var result = sut.GetList();
+
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+
+            Assert.Equal("1", result[0]);
+            Assert.Equal("2", result[1]);
+            Assert.Equal("Fizz", result[2]);
+            Assert.Equal("4", result[3]);
+            Assert.Equal("Buzz", result[4]);
+            Assert.Equal("Fizz", result[5]);
+            Assert.Equal("7", result[6]);
+            Assert.Equal("8", result[7]);
+            Assert.Equal("Fizz", result[8]);
+            Assert.Equal("Buzz", result[9]);
+        }
+
+        [Fact]
+        public void ReturnAListOfCorrectStrings_2ndVersion()
+        {
+            var sut = new FizzBuzzHelper();
+            var result = sut.GetList(FizzBuzzVersion.Verstion2, 100);
+
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+
+            Assert.Equal("1", result[0]);
+            Assert.Equal("2", result[1]);
+            Assert.Equal("Fizz", result[2]);
+            Assert.Equal("4", result[3]);
+            Assert.Equal("Buzz", result[4]);
+            Assert.Equal("Fizz", result[5]);
+            Assert.Equal("7", result[6]);
+            Assert.Equal("8", result[7]);
+            Assert.Equal("Fizz", result[8]);
+            Assert.Equal("Buzz", result[9]);
+        }
+
+        [Fact]
+        public void ReturnAListOfCorrectStrings_RuleVersion()
+        {
+            var sut = new FizzBuzzHelper();
+            var result = sut.GetList(FizzBuzzVersion.UsingRules, 100);
+
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+
+            Assert.Equal("1", result[0]);
+            Assert.Equal("2", result[1]);
+            Assert.Equal("Fizz", result[2]);
+            Assert.Equal("4", result[3]);
+            Assert.Equal("Buzz", result[4]);
+            Assert.Equal("Fizz", result[5]);
+            Assert.Equal("7", result[6]);
+            Assert.Equal("8", result[7]);
+            Assert.Equal("Fizz", result[8]);
+            Assert.Equal("Buzz", result[9]);
+        }
+    }
+}
---
+++ src/CodingChallanges/CodingChallanges/FizzBuzzHelper.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+
+namespace CodingChallanges
+{
+    public class FizzBuzzHelper
+    {
+        public List<string> GetList(FizzBuzzVersion fizzBuzzVersion = FizzBuzzVersion.Base, int maxNumber = 100)
+        {
+            switch (fizzBuzzVersion)
+            {
+                case FizzBuzzVersion.Base:
+                    return GetList_BaseVersion(maxNumber);                    
+                case FizzBuzzVersion.Verstion2:
+                    return GetList_2ndVersion(maxNumber);
+                case FizzBuzzVersion.UsingRules:
+                    return GetList_UsingRules(maxNumber);
+            }
+
+            return null;
+        }
+
+        private List<string> GetList_UsingRules(int maxNumber)
+        {
+            var resultList = new List<string>();
+
+            var ruleList = new Dictionary<string, int>
+            {
+                { "Fizz", 3 },
+                { "Buzz", 5 }
+            };
+
+            for (int i = 1; i < maxNumber; i++)
+            {
+                var newEntry = "";
+                foreach (var rule in ruleList)
+                {
+                    if (i % rule.Value == 0)
+                    {
+                        newEntry += rule.Key;
+                    }
+                }
+
+                if (string.IsNullOrEmpty(newEntry))
+                    newEntry = i.ToString();
+
+                resultList.Add(newEntry);
+            }
+
+            return resultList;
+        }
+
+        private List<string> GetList_2ndVersion(int maxNumber)
+        {
+            var resultList = new List<string>();
+            for (int i = 1; i < maxNumber; i++)
+            {
+                var newEntry = "";
+                if (i % 3 == 0)
+                {
+                    newEntry += "Fizz";
+                }
+                else if (i % 5 == 0)
+                {
+                    newEntry += "Buzz";
+                }
+                else
+                {
+                    newEntry += i.ToString();
+                }
+
+                resultList.Add(newEntry);
+            }
+
+            return resultList;
+        }
+
+        private List<string> GetList_BaseVersion(int maxNumber)
+        {
+            var resultList = new List<string>();
+            for (int i = 1; i < maxNumber; i++)
+            {
+                if (i % 5 == 0 && i % 3 == 0)
+                {
+                    resultList.Add("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    resultList.Add("Fizz");
+                }
+                else if(i % 5 == 0)
+                {
+                    resultList.Add("Buzz");
+                }
+                else
+                {
+                    resultList.Add(i.ToString());
+                }
+            }
+
+            return resultList;
+        }
+    }
+
+    public enum FizzBuzzVersion { Base, Verstion2, UsingRules }
+}
