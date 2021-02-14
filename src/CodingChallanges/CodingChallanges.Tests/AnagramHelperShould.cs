441cbbe4e62d887af2c771af9c67c3542163c9b1
Katrin Nitsche <katrin.nitsche@ecomsilver.com>
2021-02-14 14:48:53 +0000

    added anagram challange

---
+++ src/CodingChallanges/CodingChallanges.Tests/AnagramHelperShould.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CodingChallanges.Tests
+{
+    public class AnagramHelperShould
+    {
+        [Fact]
+        public void ReturnPositiv()
+        {
+            var sut = new AnagramHelper();
+            var result = sut.Compare("Test", "estT");
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ReturnNegative()
+        {
+            var sut = new AnagramHelper();
+            var result = sut.Compare("Test", "NotEqual");
+
+            Assert.False(result);
+        }
+    }
+}
---
+++ src/CodingChallanges/CodingChallanges/AnagramHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodingChallanges
+{
+    public class AnagramHelper
+    {
+        public bool Compare(string word1, string word2)
+        {
+            var listOfLetters1 = new HashSet<char>(word1.ToLower().ToList());
+            var listOfLetters2 = new HashSet<char>(word2.ToLower().ToList());
+
+            var orderedList1 = listOfLetters1.OrderBy(x => x);
+            var orderedList2 = listOfLetters2.OrderBy(x => x);
+
+            if (orderedList1.Count() != orderedList2.Count()) return false;
+
+            for (int i = 0; i < orderedList1.Count(); i++)
+            {
+                if (orderedList1.ElementAt(i) != orderedList2.ElementAt(i)) return false;
+            }
+
+            return true;
+        }
+    }
+}
