40285c0fc322e95f76119f34eea2efbd9c22d7e4
Katrin Nitsche <katrin.nitsche@ecomsilver.com>
2021-02-15 06:18:20 +0000

    added Fibonacci challange

---
+++ src/CodingChallanges/CodingChallanges.Tests/FibonacciHelperShould.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace CodingChallanges.Tests
+{
+    public class FibonacciHelperShould
+    {
+        // 0	1	1	2	3	5	8	13	21	34	55	89	144	233	377	610	987	1597	2584	4181	6765
+        [Theory]
+        [InlineData(3, 2)]
+        [InlineData(9, 34)]
+        [InlineData(20, 6765)]
+        public void ReturnTheNthNumber(int number, int expectedValue)
+        {
+            var sut = new FibonacciHelper();
+            var result = sut.GetNumber(number);
+
+            Assert.Equal(expectedValue, result);
+        }
+
+        [Theory]
+        [InlineData(4, new int[] { 0, 1, 1, 2 })]
+        [InlineData(10, new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 })]
+        public void ReturnTheSequence(int number, int[] expectedSequence)
+        {
+            var sut = new FibonacciHelper();
+            var result = sut.GetSequenceUpTo(number);
+
+            Assert.NotNull(result);
+            Assert.Equal(expectedSequence, result);
+        }
+    }
+}
---
+++ src/CodingChallanges/CodingChallanges/FibonacciHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodingChallanges
+{
+    public class FibonacciHelper
+    {
+        private List<int> previousResult = new List<int>();
+
+        public int GetNumber(int v)
+        {
+            if (v == 0 || v == 1) return v;
+            return GetNumber(v - 1) + GetNumber(v - 2);
+        }
+
+        public int[] GetSequenceUpTo(int v)
+        {
+            var result = new List<int>();
+
+            for (int i = 0; i < v; i++)
+            {
+                if (previousResult.Count > 0 && previousResult.Count == i - 1)
+                {
+                    result.Add(previousResult[i]);
+                }
+                else
+                {
+                    var nextResult = GetNumber(i);
+                    previousResult.Add(nextResult);
+                    result.Add(nextResult);
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
